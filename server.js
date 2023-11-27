a40363acabfe33ec154c070a1fc6181ee9afc285
Hy Nguyen <giahy316@gmail.com>
2023-11-27 00:56:08 -0700

    backend work

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const {MongoClient} = require('mongodb');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt')
+
 
 const fileExtenions = {
     '.html': 'text/html',
@@ -47,15 +49,15 @@ app.get('/products', (req, res) => {
     res.sendFile(path.join(__dirname, 'products.html'));
 });
 
-const database = 'MetaPC';
-const contact_name = 'contact_form';
+const db = client.db('MetaPC');
+const usersCollection = db.collection("users")
+const contact_collection = db.collection('contact_form');
+
 // Start the Express server
 client.connect()
   .then(() => {
-    const db = client.db(database);
-    contact_collection = db.collection(contact_name);
     app.listen(8000, () => {
-      console.log(`The web server is alive! \nListening on http://localhost:8000`);
+      console.log(`The web server is alive! \nListening on http://localhost:8000/login.html`);
     });
   })
   .catch((err) => {
@@ -72,5 +74,45 @@ app.post('/contact', async (req, res) => {
     } catch(err) {
         console.error(err);
         res.json({ success: false });  
+    }
+});
+
+// Login Input Validation
+app.post('/login', async (req, res) => {
+    const {user_Email, user_Password} = req.body;
+
+    try {
+        // Find user in the MongoDB database
+
+        const user = await usersCollection.findOne({email: req.body.email});
+
+        if (!user) {
+            return res.json({ success: false, message: 'UserNotFound' });
+        } else {
+            const passwordMatch = (req.body.password === user.password)
+            // bcrypt is a library to hash and compare passwords
+            if (!passwordMatch) {
+                return res.json({ success:false, message: 'Invalid password' });
+            }
+        }
+       
+        return res.json({ success: true });
+    } catch(err) {
+        console.error(err);
+        res.json({ success: false });  
+    }
+});
+
+// Sign Up Processing
+app.post('/signup', async (req, res) => {
+    // collection = usersCollection
+    const {firstName, lastName, email, address, password} = req.body;
+    try {
+        console.log(firstName);
+        console.log(req.body);
+        usersCollection.insertOne(req.body)
+        res.json({ success: true, message: 'User created successfully!'});
+    } catch (err) {
+        res.json({ success: false, error: err.message });
     }
 });
