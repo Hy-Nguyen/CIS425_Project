1a3104e223e1d7e24043468949b42aca55bc5e3c
Hy Nguyen <giahy316@gmail.com>
2023-11-29 04:13:13 -0700

    l

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const fileExtenions = {
-//     '.html': 'text/html',
-//     '.css': 'text/css',
-//     '.js': 'text/javascript',
-//     '.jpeg': 'text/jpeg',
-//     '.jpg': 'text/kpeg',
-//     '.png': 'text/png'
-// };
